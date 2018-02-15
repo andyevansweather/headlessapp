7ea59175c6add4758ebe315fd60f9832f7a626bb
Andy Evans <andrew.evans@metoffice.gov.uk>
2018-02-15 10:43:27 +0000

    added more files

--- routes/generatepdf.js
+++ routes/generatepdf.js
@@ -4,6 +4,8 @@ const router = express.Router();
 const htmlPdf = require('html-pdf-chrome');
  
 const html = '<p>Hello, world!</p>';
+const url = 'https://github.com/westy92/html-pdf-chrome';
+
 const options = {
   port: 9222, // port Chrome is listening on
 };
@@ -12,12 +14,9 @@ const options = {
 router.get('/', function(req, res, next) {
 
   // Generating pdf
-  htmlPdf.create(html, options)
-    .then(pdf => pdf.toFile('test.pdf'))
+  htmlPdf.create(url, options)
+    .then(pdf => pdf.toFile('new.pdf'))
     .catch(err => console.log(err));
-  // htmlPdf.create(html, options).then((pdf) => pdf.toBase64());
-  // htmlPdf.create(html, options).then((pdf) => pdf.toBuffer());
-
 
   res.send('tada pdf made!!');
 });
