f6bc274d27bb2a68405cc33312f93ec6b64f8ce7
Andy Evans <andrew.evans@metoffice.gov.uk>
2018-02-15 09:54:18 +0000

    added more changes

--- readme.md
+++ readme.md
@@ -3,3 +3,7 @@
 This is a nodejs application made with express generator for testing html to pdf with headless chrome.
 
 This is made from the link https://www.npmjs.com/package/html-pdf-chrome
+
+## Run Chrome Headless
+
+chrome --headless --remote-debugging-port=9222 https://chromium.org
--- routes/generatepdf.js
+++ routes/generatepdf.js
@@ -1,8 +1,24 @@
 const express = require('express');
 const router = express.Router();
 
+const htmlPdf = require('html-pdf-chrome');
+ 
+const html = '<p>Hello, world!</p>';
+const options = {
+  port: 9222, // port Chrome is listening on
+};
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+
+  // Generating pdf
+  htmlPdf.create(html, options)
+    .then(pdf => pdf.toFile('test.pdf'))
+    .catch(err => console.log(err));
+  // htmlPdf.create(html, options).then((pdf) => pdf.toBase64());
+  // htmlPdf.create(html, options).then((pdf) => pdf.toBuffer());
+
+
   res.send('tada pdf made!!');
 });
 
